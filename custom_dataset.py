177543a9e52df18d9adbc29dc7d5c7ae4971b40e
vince <43069944+iamvincenzo@users.noreply.github.com>
2023-05-19 21:59:31 +0200

    update custom_dataset.py

--- custom_dataset.py
+++ custom_dataset.py
@@ -5,12 +5,18 @@ from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
 
 class CustomDataset(Dataset):
-    def __init__(self):
+    def __init__(self, x, seq_len):
         super().__init__()
-        pass
+        self.x = x
+        self.seq_len = seq_len
 
     def __getitem__(self, index):
-        pass
+        start_index = index * self.seq_len
+        end_index = start_index + self.seq_len
+        data_x = self.x[start_index:end_index, :]
+        tensor_x = torch.from_numpy(data_x)
 
-    def get_lenght(self):
-        pass
+        return tensor_x
+
+    def __len__(self):
+        return len(self.x) // self.seq_len
