26da4a0a256a2ac349866d685905f77a6c9729d1
vince <43069944+iamvincenzo@users.noreply.github.com>
2023-05-19 22:34:12 +0200

    update custom_dataset.py

--- custom_dataset.py
+++ custom_dataset.py
@@ -5,7 +5,7 @@ import pandas as pd
 from torch.utils.data import Dataset
 
 """ Function used to import data from csv files. """
-def get_data(data_path):
+def get_data(data_path, seq_len, verbose=False):
     # import data into dataframe
     df_train = pd.read_csv(os.path.join(data_path, "training_ready.csv"))
     df_valid = pd.read_csv(os.path.join(data_path, "validation_ready.csv"))
@@ -21,28 +21,28 @@ def get_data(data_path):
     df_valid.drop(columns=["fyear", "cik", "status_label", "status"], inplace=True)
     df_test.drop(columns=["fyear", "cik", "status_label", "status"], inplace=True)
 
-    # # print for debugging
-    # print(f"\ndf_train: \n{df_train.head(5)}\n")
-    # print(f"\ndf_valid: \n{df_valid.head(5)}")
-    # print(f"\ndf_test: \n{df_test.head(5)}")
+    if verbose:
+        print(f"\ndf_train: \n{df_train.head(5)}\n")
+        print(f"\ndf_valid: \n{df_valid.head(5)}")
+        print(f"\ndf_test: \n{df_test.head(5)}")
 
-    # print(f"\ndf_train-shape: \n{df_train.shape}")
-    # print(f"\ndf_valid-shape: \n{df_valid.shape}")
-    # print(f"\ndf_test-shape: \n{df_test.shape}")
+        print(f"\ndf_train-shape: \n{df_train.shape}")
+        print(f"\ndf_valid-shape: \n{df_valid.shape}")
+        print(f"\ndf_test-shape: \n{df_test.shape}")
 
-    # print(f"\ntotal train-samples: \n{df_train.shape[0]//args.seq_len}")
-    # print(f"\ntotal valid-samples: \n{df_valid.shape[0]//args.seq_len}")
-    # print(f"\ntotal test-samples: \n{df_test.shape[0]//args.seq_len}")
+        print(f"\ntotal train-samples: \n{df_train.shape[0]//seq_len}")
+        print(f"\ntotal valid-samples: \n{df_valid.shape[0]//seq_len}")
+        print(f"\ntotal test-samples: \n{df_test.shape[0]//seq_len}")
 
     # convert data into numpy array
     np_train = df_train.to_numpy(copy=False, dtype=np.float32)
     np_valid = df_valid.to_numpy(copy=False, dtype=np.float32)
     np_test = df_test.to_numpy(copy=False, dtype=np.float32)
 
-    # # print for debugging
-    # print(f"\nnp_train-shape: \n{np_train.shape}")
-    # print(f"\nnp_valid-shape: \n{np_valid.shape}")
-    # print(f"\nnp_test-shape: \n{np_test.shape}")
+    if verbose:
+        print(f"\nnp_train-shape: \n{np_train.shape}")
+        print(f"\nnp_valid-shape: \n{np_valid.shape}")
+        print(f"\nnp_test-shape: \n{np_test.shape}")
 
     return np_train, np_valid, np_test
 
