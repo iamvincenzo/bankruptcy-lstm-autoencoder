b07de7130c1a6e23005860185d0cc2b0bdfb6bd3
vince <43069944+iamvincenzo@users.noreply.github.com>
2023-05-20 13:18:17 +0200

    update solver.py

--- solver.py
+++ solver.py
@@ -9,23 +9,24 @@ from metrics import Rec_Loss
 from pytorchtools import EarlyStopping
 from pltotting_utils import plot_losses
 
-from models import EncoderDecoderLSTM
-from models import DenseSoftmaxLayer
+
 
 """ Class for training, validation and testing. """
 class Solver(object):
     """ Initialize configuration. """
-    def __init__(self, model, train_loader, valid_loader, test_loader, writer, device, args):
+    def __init__(self, models, train_loader, valid_loader, test_loader, writer, device, args):
         super().__init__()
         self.args = args
         self.model_name = f"lstm_encdec_{self.args.model_name}.pt"
 
-        # PART-1
-        ###################################################################
-        self.autoencoder = model.to(device)
+        # models to train/validate
+        self.autoencoder, self.dense90, self.dense5 = models
+        self.autoencoder = self.autoencoder.to(device)
+        self.dense90 = self.dense90.to(device)
+        self.dense5 = self.dense5.to(device)
 
         # load a pretrained model
-        if self.args.resume_train:
+        if self.args.resume_train or not(self.args.train_only_ae):
             self.load_model(self.autoencoder, device)
 
         # select the loss for optimization
@@ -34,6 +35,9 @@ class Solver(object):
         elif self.args.loss == "mse_loss":
             self.criterion = nn.MSELoss()
 
+        self.criterion1 = nn.CrossEntropyLoss()
+        self.criterion2 = nn.CrossEntropyLoss()
+
         # choose the optimizer
         if self.args.opt == "Adam":
             self.optimizer = optim.Adam(params=self.autoencoder.parameters(),
@@ -41,8 +45,13 @@ class Solver(object):
         elif self.args.opt == "SGD":
             self.optimizer = optim.SGD(params=self.autoencoder.parameters(),
                                        lr=self.args.lr, momentum=0.9)
+            
+        self.optimizer1 = optim.SGD(params=self.dense90.parameters(), 
+                                    lr=0.001, momentum=0.9)
+        self.optimizer2 = optim.SGD(params=self.dense5.parameters(), 
+                                    lr=0.001, momentum=0.9)
 
-        self.num_classes = self.args.num_classes
+        # other training/validation params
         self.num_epochs = args.num_epochs
         self.patience = self.args.patience
         self.train_loader = train_loader
@@ -52,29 +61,9 @@ class Solver(object):
         self.device = device
 
         # visualize the model we built on tensorboard
-        x = next(iter(self.train_loader))
-        self.writer.add_graph(self.autoencoder, x.to(self.device))
-        self.writer.close()
-        ###################################################################
-
-        # PART-2
-        ###################################################################    
-        input_dim = x.size(2) * x.size(1)
-        output_dim = x.size(1)
-        self.dense90 = DenseSoftmaxLayer(input_dim=input_dim, 
-                                         output_dim=output_dim)
-        
-        input_dim = x.size(1)
-        output_dim = self.num_classes
-        self.dense5 = DenseSoftmaxLayer(input_dim=input_dim, 
-                                        output_dim=output_dim)
-        
-        self.optimizer1 = optim.SGD(params=self.dense90.parameters(), lr=0.001, momentum=0.9)
-        self.optimizer2 = optim.SGD(params=self.dense5.parameters(), lr=0.001, momentum=0.9)
-
-        self.criterion1 = nn.CrossEntropyLoss()
-        self.criterion2 = nn.CrossEntropyLoss()
-        ###################################################################
+        x, _ = next(iter(self.train_loader))
+        self.writer.add_graph(self.autoencoder, x.to(device))
+        self.writer.close()        
 
     """ Method used to load a model. """
     def load_model(self, model, device):
@@ -83,10 +72,8 @@ class Solver(object):
                                          map_location=torch.device(device=device)))
         print("\nModel loaded...")
 
-    # PART-1
-    ##############################################################################
     """ Method used to train the autoencoder model with early stopping implementation. """
-    def train_autoencoder(self):
+    def train_ae(self):
         print(f"\nStarting autoencoder training...")
 
         # to track the training loss as the model trains
@@ -108,14 +95,13 @@ class Solver(object):
 
         # loop over the dataset multiple times
         for epoch in range(self.num_epochs):
-            print(
-                f"\nTraining iteration | Epoch[{epoch + 1}/{self.num_epochs}]\n")
+            print(f"\nTraining iteration | Epoch[{epoch + 1}/{self.num_epochs}]\n")
 
             # used for creating a terminal progress bar
             loop = tqdm(enumerate(self.train_loader),
                         total=len(self.train_loader), leave=True)
 
-            for batch, X in loop:
+            for batch, (X, _) in loop:
                 # put data on correct device
                 X = X.to(self.device)
 
@@ -126,7 +112,7 @@ class Solver(object):
                 enc_output, dec_output = self.autoencoder(X)
 
                 # calculate the loss
-                loss = self.criterion(dec_output, X)
+                loss = self.criterion(X, dec_output)
                 # backward pass: compute gradient of the loss with respect to model parameters
                 loss.backward()
                 # perform a single optimization step (parameter update)
@@ -136,7 +122,7 @@ class Solver(object):
                 train_losses.append(loss.item())
 
             # validate the model at the end of each epoch
-            self.test_autoencoder(epoch, valid_losses)
+            self.test_ae(epoch, valid_losses)
 
             # # update the learning rate scheduler
             # self.scheduler.step()
@@ -179,33 +165,28 @@ class Solver(object):
         print("\nFinished training...\n")
 
     """ Method used to evaluate the model on the validation set. """
-    def test_autoencoder(self, epoch, test_losses):
-        print(
-            f"\nEvaluation iteration | Epoch [{epoch + 1}/{self.num_epochs}]\n")
+    def test_ae(self, epoch, valid_losses):
+        print(f"\nEvaluation iteration | Epoch [{epoch + 1}/{self.num_epochs}]\n")
 
         # put model into evaluation mode
         self.autoencoder.eval()
 
         # no need to calculate the gradients for our outputs
         with torch.no_grad():
-            test_loop = tqdm(enumerate(self.valid_loader),
-                             total=len(self.valid_loader), leave=True)
+            loop = tqdm(enumerate(self.valid_loader), total=len(self.valid_loader), leave=True)
 
-            for _, test_X in test_loop:
-                test_X = test_X.to(self.device)
+            for _, (X, _) in loop:
+                X = X.to(self.device)
 
-                test_enc_output, test_dec_output = self.autoencoder(test_X)
+                enc_output, dec_output = self.autoencoder(X)
 
-                test_loss = self.criterion(test_dec_output, test_X)
+                test_loss = self.criterion(X, dec_output)
 
-                test_losses.append(test_loss.item())
+                valid_losses.append(test_loss.item())
 
         # reput model into training mode
         self.autoencoder.train()
-    ##############################################################################
 
-    # PART-2
-    ##############################################################################
     if True:
         def compute_dot_product(self, mat1, mat2):
             # # Create the input tensors
@@ -246,7 +227,7 @@ class Solver(object):
 
         #     print("Training finished.")
 
-        def train_entire_model(self):
+        def train_all(self):
             print(f"\nStarting autoencoder training...")
 
             self.load_model(self.autoencoder, self.device)
@@ -282,6 +263,5 @@ class Solver(object):
                     pass
                     # x = x.reshape((x.size(0), x.size(2) * x.size(1)))
 
-        def test_entire_model(self):
+        def test_all(self):
             pass
-    ##############################################################################
