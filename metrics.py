4b0c45960e2433682ec4303280f2cdc00a44629d
vince <43069944+iamvincenzo@users.noreply.github.com>
2023-05-21 11:44:46 +0200

    update metrics.py

--- metrics.py
+++ metrics.py
@@ -10,8 +10,8 @@ class Rec_Loss(nn.Module):
         super(Rec_Loss, self).__init__()
     
     """ Method used to compute the loss value. """
-    def forward(self, y_true, y_pred):
-        out = torch.sub(y_true, y_pred).pow(2)
+    def forward(self, y_pred, y_true):
+        out = torch.sub(y_pred, y_true).pow(2)
         # # dim = 2 indicates to pick columns 
         # # element per row to compute the mean per row
         out = torch.mean(input=out, dim=2)  # , keepdim=True)
@@ -21,6 +21,59 @@ class Rec_Loss(nn.Module):
         return out
 
 
+# https://gist.github.com/the-bass/cae9f3976866776dea17a5049013258d
+def confusion(prediction, truth):
+    """ Returns the confusion matrix for the values in the `prediction` and `truth`
+    tensors, i.e. the amount of positions where the values of `prediction`
+    and `truth` are
+    - 1 and 1 (True Positive)
+    - 1 and 0 (False Positive)
+    - 0 and 0 (True Negative)
+    - 0 and 1 (False Negative)
+    """
+
+    confusion_vector = prediction / truth
+    # Element-wise division of the 2 tensors returns a new tensor which holds a
+    # unique value for each case:
+    #   1     where prediction and truth are 1 (True Positive)
+    #   inf   where prediction is 1 and truth is 0 (False Positive)
+    #   nan   where prediction and truth are 0 (True Negative)
+    #   0     where prediction is 0 and truth is 1 (False Negative)
+
+    true_positives = torch.sum(confusion_vector == 1).item()
+    false_positives = torch.sum(confusion_vector == float('inf')).item()
+    true_negatives = torch.sum(torch.isnan(confusion_vector)).item()
+    false_negatives = torch.sum(confusion_vector == 0).item()
+
+    return true_positives, false_positives, true_negatives, false_negatives
+
+""" Method used to compute accuracy, precision and recall. """
+def compute_metrics(predictions, targets):
+    # returns the indices of the maximum value of all elements in the input tensor (dim=1) by row
+    predicted_classes = torch.argmax(predictions, dim=1)
+
+    # correct_predictions = (predicted_classes == targets).sum().item()
+    tp = torch.sum((predicted_classes == 1) & (targets == 1)).item()
+    fp = torch.sum((predicted_classes == 1) & (targets == 0)).item()
+    fn = torch.sum((predicted_classes == 0) & (targets == 1)).item()
+    tn = torch.sum((predicted_classes == 0) & (targets == 0)).item()
+
+    # accuracy = (correct_predictions / targets.size(0))
+    # 1e-10 is a small value commonly used to avoid division by zero
+    accuracy = (tp + tn) / (tp + fp + fn + tn) # + 1e-10)        
+    precision = tp / (tp + fp + 1e-10)
+    recall = tp / (tp + fn + 1e-10)
+    f1_score = (2 * precision * recall) / (precision + recall + 1e-10)
+    specificity = tn / (tn + fp)
+    
+    # tp, fp, tn, fn = confusion(predicted_classes, targets)
+    # print(tp, fp, fn, tn)
+
+    # create the confusion matrix
+    confusion_matrix = torch.tensor([[tp, fp], 
+                                     [fn, tn]])
+
+    return accuracy, precision, recall, f1_score, specificity, confusion_matrix
 
 
 
